b8e2777d43ba3cb21721aeb8ff568bcf1b6f3790
Gi345byte <113916871+Gi345byte@users.noreply.github.com>
2025-12-23 13:47:53 +0530

    updated animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import BlogNew from "./Blognew";
 import BlogPage from "./components/BlogPage";
 
-export default function App() {
+function App() {
   return (
-    <>
+    <div className="min-h-screen bg-gray-50 text-gray-900">
       <BlogPage />
-    </>
+      
+    </div>
   );
 }
+
+export default App;
